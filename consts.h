70232cdedc0cfbcdd10bc07cb90bb4e91e9eb239
texruska <texruska@users.noreply.github.com>
2018-11-27 16:32:13 +0000

    Add makefile

--- consts.h
+++ consts.h
@@ -5,8 +5,8 @@ namespace RayTracer {
 
 const float GLOBAL_SETTING_RAY_PRECISION = 0.0000000000001;	// how close a ray a ray has to be to a surface before it is considered intersecting
 const int GLOBAL_SETTING_RAY_MAX_LIFE = 1;		// how many times a ray will recurse
-const float GLOBAL_SETTING_RAY_SPREAD = 30;		// how much rays should scatter at each step (degrees)
-const int GLOBAL_SETTING_RAY_NUM_SPAWN = 100;		// how many rays should be spawned at each step
+const float GLOBAL_SETTING_RAY_SPREAD = 10;		// how much rays should scatter at each step (degrees)
+const int GLOBAL_SETTING_RAY_NUM_SPAWN = 2;		// how many rays should be spawned at each step
 
 }
 
